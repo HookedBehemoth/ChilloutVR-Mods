fda76d2f2a8fc01febb3b380044f0c8d585407c2
HookedBehemoth <LuisScheurenbrand@gmail.com>
2022-08-01 18:53:15 +0200

    add video skip

---
+++ NoVideo/NoVideo.cs
@@ -0,0 +1,18 @@
+﻿using MelonLoader;
+using UnityEngine;
+
+[assembly: MelonInfo(typeof(NoVideo.Starter), nameof(NoVideo), "1.0", "Behemoth")]
+[assembly: MelonGame("Alpha Blend Interactive", "ChilloutVR")]
+
+namespace NoVideo;
+public class Starter : MelonMod
+{
+    public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+    {
+        if (sceneName == "Login")
+        {
+            GameObject.Find("LoginCanvas/BG (1)").SetActive(false);
+            GameObject.Find("LoginCanvas/BG").SetActive(false);
+        }
+    }
+}
